b91c9126b1f3bfcafa19729eea1eba8f27f8f2ff
Alexis VIALARET <alexisvlrt@gmail.com>
2020-07-01 23:08:38 +0200

    (feat) logger

---
+++ lib/api/logger.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from pathlib import Path
+
+
+def log_request(request, status, processing_time):
+    entry = format_entry(request, status, processing_time)
+    log(entry)
+
+
+def format_entry(request, status, processing_time):
+    client_ip = request.environ.get('REMOTE_ADDR')
+    parameters = {param: request.query.get(param) for param in list(request.query)}
+    processing_time = round(processing_time, 3)
+    return f"{datetime.now().isoformat()} | {client_ip} | {status} | {processing_time}s | Parameters: {parameters}\n"
+
+
+def log(entry):
+    with open(Path(__file__).parent.parent.parent / "log", "a+") as log_file:
+        log_file.write(entry)
