ddcf24ecd0b03345f0d8077b42828f38485d5fa1
Alexis VIALARET <alexisvlrt@gmail.com>
2020-07-04 18:13:23 +0200

    (feat) Help page for the API

--- bin/main.py
+++ bin/main.py
@@ -1,7 +1,8 @@
 import json
 import time
+from pathlib import Path
 
-from bottle import route, run, request, HTTPResponse
+from bottle import route, run, request, HTTPResponse, static_file
 
 from lib.api.entrypoint import Entrypoint
 from lib.api.exceptions import ApiBaseException
@@ -22,6 +23,10 @@ def main():
     return response
 
 
-entry_point = Entrypoint(config.ALLOWED_PARAMETERS, config.PARAMETERS_GROUPS)
+@route('/blackfalcon')
+def help():
+    return static_file("help_page.html", root=Path(__file__).parent.parent / "lib" / "api")
 
+
+entry_point = Entrypoint(config.ALLOWED_PARAMETERS, config.PARAMETERS_GROUPS)
 run(host="0.0.0.0", port=80, server='gunicorn', workers=1)
